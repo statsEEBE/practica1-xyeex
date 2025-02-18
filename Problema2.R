45b9abf900c47fffc480a5b46abaddfb540f5fbb
xyeex <wangjiahui.ye@estudiantat.upc.edu>
2025-02-18 09:51:08 +0100

    sol_Problema2.R

--- Problema2.R
+++ Problema2.R
@@ -5,3 +5,45 @@ rm(list=ls()) #limpiar/Borrar los dados anteriores
 data<-mtcars
 hist(data$mpg) #zhuzhuangtu
 #plot(data$mpg)#dianzhengtu
+
+
+
+
+mis_dades <- mtcars
+
+rm(list=ls()) #limpiar/Borrar los dados anteriores 
+data<-mtcars
+x <- data$cyl
+#frequencia absoluta
+ni <- table(x)
+ni
+barplot(ni)
+
+#frequencia absoluta acumulada
+Ni <- cumsum(ni)
+ni
+Ni
+
+#frequencia relativa
+fi <- ni/length(x)
+fi
+pie(fi) #grafico de sectores
+sum(fi)
+
+#frecuencia relativa acumulada
+Fi <- cumsum(fi)
+fi
+Fi
+
+cbind(ni, fi, Ni, Fi) #la tabla de frecuencia 
+
+data$mpg
+x <- cut(data$mpg,10)
+ni <- table(x)
+Ni <- cumsum(ni)
+fi <- ni/length(x)
+Fi <- cumsum(fi)
+cbind(ni, fi, Ni, Fi)
+barplot(ni)
+
+
