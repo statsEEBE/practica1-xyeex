c27685df1df6fd16c8f8a22d93a7fda59d833eb5
xyeex <wangjiahui.ye@estudiantat.upc.edu>
2025-02-18 09:58:19 +0100

    sol_Problema3.R

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,12 @@
 #Codigo para problema 3
+c(-8, 5, 2, -8, 9, 5, 2, -3, 1, -1, 4, -4, 9, 3, -9, 7, 0, -7, 8, -4, 1, 7, -6, 4, 5, -9, -2, -8, 5, -5)
+x<-c(-8, 5, 2, -8, 9, 5, 2, -3, 1, -1, 4, -4, 9, 3, -9, 7, 0, -7, 8, -4, 1, 7, -6, 4, 5, -9, -2, -8, 5, -5)
+x
+sum(x)
+#para encontrar el segundo numero del vector, x[2], utiliza "[]"
+#para elinimar algunos numeros x[-c(...)]
+y<- x[-c(21, 2, 26)]
+y
+sum(exp(x))- sum(exp(y))
+y[19]
+y[24]
